1f7dc2b77bd41b493e709546a8869223c3f87d8c
Evan boissonnot <evan@boissonnot.fr>
2026-06-23 17:02:36 +0200

    feat: signal

--- projects/front-end/src/app/app.ts
+++ projects/front-end/src/app/app.ts
@@ -1,4 +1,4 @@
-import { Component, signal } from '@angular/core';
+import { Component, computed, OnInit, signal } from '@angular/core';
 import { DisplayPlaceList } from './features/places/components/display-place-list/display-place-list';
 import { DisplayOneDestination } from './features/destinations/components/display-one-destination/display-one-destination';
 import { DisplayCurrentWeather } from './features/weather/components/display-current-weather/display-current-weather';
@@ -10,12 +10,20 @@ import { ObsDiscover } from './features/learnings/obs-discover/obs-discover';
   templateUrl: './app.html',
   styleUrl: './app.css',
 })
-export class App {
-  protected readonly title = signal('front-end');
+export class App implements OnInit {
+  protected readonly title = signal<string | undefined>('Mes vacances');
+  protected readonly titleAsMaj = computed(() => this.title()?.toUpperCase());
 
   protected alive = true;
 
   togleAlive(): void {
     this.alive = !this.alive;
+  }
+
+  ngOnInit(): void {
+    setTimeout(() => {
+      //this.title.set('Mes super vacances');
+      this.title.update((oldValue) => oldValue + ' super');
+    }, 2000);
   }
 }
