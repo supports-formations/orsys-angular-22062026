fff0454b2ab96c37b93504ee031a2f3c6bd46c4a
Evan boissonnot <evan@boissonnot.fr>
2026-06-23 10:32:36 +0200

    feat: double pipe

--- projects/front-end/src/app/features/destinations/components/display-one-destination/display-one-destination.ts
+++ projects/front-end/src/app/features/destinations/components/display-one-destination/display-one-destination.ts
@@ -2,10 +2,11 @@ import { Component, inject, OnInit } from '@angular/core';
 import { GetOneDestination } from '../../services/get-one-destination';
 import { Destination } from '../../models';
 import { StepToStringPipe } from '../../uis/pipes/step-to-string-pipe';
+import { UpperCasePipe } from '@angular/common';
 
 @Component({
   selector: 'orsys-display-one-destination',
-  imports: [StepToStringPipe],
+  imports: [StepToStringPipe,UpperCasePipe],
   templateUrl: './display-one-destination.html',
   styleUrl: './display-one-destination.css',
   //providers: [GetOneDestination]
