5c484ba53d57547b2f54fc00da81edd2c641a0f5
Evan boissonnot <evan@boissonnot.fr>
2026-06-23 11:25:04 +0200

    feat: delay operator

--- projects/front-end/src/app/features/places/services/get-list-places.ts
+++ projects/front-end/src/app/features/places/services/get-list-places.ts
@@ -1,7 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { inject, Service } from '@angular/core';
 import { Place } from '../models/place';
-import { filter, map, Observable, retry } from 'rxjs';
+import { delay, filter, map, Observable, retry } from 'rxjs';
 
 @Service()
 export class GetListPlaces {
@@ -11,7 +11,8 @@ export class GetListPlaces {
    return this.httpClient.get<Place[]>('/places.json').pipe(
     retry({ count: 3, delay: 1000 }),
     //filter((places) => places.length > 0),
-    map((places) => places.sort((a, b) => a.label.localeCompare(b.label)))  
+    map((places) => places.sort((a, b) => a.label.localeCompare(b.label))),
+    delay(1000)
    ); // return d'un moteur qui doit être souscrit pour être exécuté
  }
 }
