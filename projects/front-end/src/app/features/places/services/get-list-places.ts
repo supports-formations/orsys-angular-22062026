b59197ac878621be44e8057bdc9eb28da5bedc15
Evan boissonnot <evan@boissonnot.fr>
2026-06-23 11:21:35 +0200

    feat: operator rxjs

--- projects/front-end/src/app/features/places/services/get-list-places.ts
+++ projects/front-end/src/app/features/places/services/get-list-places.ts
@@ -1,13 +1,17 @@
 import { HttpClient } from '@angular/common/http';
 import { inject, Service } from '@angular/core';
 import { Place } from '../models/place';
-import { Observable } from 'rxjs';
+import { filter, map, Observable, retry } from 'rxjs';
 
 @Service()
 export class GetListPlaces {
  private readonly httpClient = inject(HttpClient);  // Singleton, injecté par Angular, permet de faire des requêtes HTTP
  
  getList(): Observable<Place[]> {
-   return this.httpClient.get<Place[]>('/places.json'); // return d'un moteur qui doit être souscrit pour être exécuté
+   return this.httpClient.get<Place[]>('/places.json').pipe(
+    retry({ count: 3, delay: 1000 }),
+    //filter((places) => places.length > 0),
+    map((places) => places.sort((a, b) => a.label.localeCompare(b.label)))  
+   ); // return d'un moteur qui doit être souscrit pour être exécuté
  }
 }
